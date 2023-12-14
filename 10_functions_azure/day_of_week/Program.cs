ba98b36b8536689f75460c78e46095292eb89f62
dimey <departmentservice17@gmail.com>
2023-12-14 19:49:41 +0200

    add home work: random phrase, day of week, info about request

---
+++ 10_functions_azure/day_of_week/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_of_week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine(currentDateTime);
+            Console.WriteLine(currentDateTime.DayOfWeek);
+
+
+            string[] arr =new string[] {"1 - Many hands make light work.",
+                "2 - Strike while the iron is hot.",
+                "3 - Honesty is the best policy.",
+                "4 - The grass is always greener on the other side of the fence.",
+                "5 - Don’t judge a book by its cover." };
+
+            Random rnd = new Random();
+            int value = rnd.Next(1, 5);
+            Console.WriteLine(value);
+        }
+    }
+}
--- 10_functions_azure/function_test_api/Function1.cs
+++ 10_functions_azure/function_test_api/Function1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,76 +7,65 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace function_test_api
 {
     public static class Function1
     {
+        
         [FunctionName("Function1")]
+
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            string[] arr = new string[] {"1 - Many hands make light work.",
+                "2 - Strike while the iron is hot.",
+                "3 - Honesty is the best policy.",
+                "4 - The grass is always greener on the other side of the fence.",
+                "5 - Don’t judge a book by its cover." };
+
+            Random rnd = new Random();
+            int value = rnd.Next(1, 5);
+
             log.LogInformation("C# HTTP trigger function processed a request.");
-            
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-    }
-}
-
-/* Cheb code
- 
- using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-
-namespace techreq
-{
-    public static class techreq
-    {
-        [FunctionName("techreq")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            
-            log.LogInformation("Lenght: " + req.Path.ToString());
+            //log.LogInformation(req.Path);
+            //log.LogInformation("1 Lenght: " + req.Path.ToString());
 
             DateTime currentDateTime = DateTime.Now;
-            log.LogInformation("Current date and time: " + currentDateTime);
+            //log.LogInformation(currentDateTime.ToString());
+            log.LogInformation("Today is " + currentDateTime.DayOfWeek.ToString());
 
             string name = req.Query["name"];
-
+            
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                ? $"Sorry, parameter is empty"
+                : $"Hello, {name}\n\n" +
+                $"Random phrase:\n {arr[value]}\n\n" +
+                $"Today is {currentDateTime.DayOfWeek.ToString()}\n\n" +
+                $"Path: {req.Path.ToString()}\n" +
+                $"Text: {req.Body.ToString()}\n" +
+                $"Title: {req.ContentLength.ToString()}\n" +
+                //$"{req.ContentType.ToString()}\n";// +
+                //$"{req.Cookies.ToString()}\n";// +
+                //$"{req.HasFormContentType.ToString()}\n" +
+                //$"{req.Headers.ToString()}\n" +
+                $"Host: {req.Host.ToString()}\n" +
+                $"HttpContext: {req.HttpContext.ToString()}\n" +
+                $"IsHttps: {req.IsHttps.ToString()}\n" +
+                $"Method: {req.Method.ToString()}\n" +
+                $"Protocol: {req.Protocol.ToString()}\n";// +
+                //$"{req.Protocol.ToString()}\n" +
+                //$"{req.Query.ToString()}\n" +
+                //$"{req.QueryString.ToString()}\n" +
+                //$"{req.Scheme.ToString()}\n";
 
             return new OkObjectResult(responseMessage);
         }
     }
 }
- 
- */
