1285f714f4347e1225fab2785919c870c3e75f61
dimey <departmentservice17@gmail.com>
2023-12-14 16:50:48 +0200

    add project function_google_cloud_01

--- 10_functions_azure/function_test_api/Function1.cs
+++ 10_functions_azure/function_test_api/Function1.cs
@@ -18,6 +18,51 @@ namespace function_test_api
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
+/* Cheb code
+ 
+ using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace techreq
+{
+    public static class techreq
+    {
+        [FunctionName("techreq")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            
+            log.LogInformation("Lenght: " + req.Path.ToString());
+
+            DateTime currentDateTime = DateTime.Now;
+            log.LogInformation("Current date and time: " + currentDateTime);
 
             string name = req.Query["name"];
 
@@ -33,3 +78,5 @@ namespace function_test_api
         }
     }
 }
+ 
+ */
