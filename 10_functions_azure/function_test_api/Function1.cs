277ca2f547fab99534681c173a576d0624db3b12
dimey <departmentservice17@gmail.com>
2023-12-15 16:27:29 +0200

    finish project: serializer_json

--- 10_functions_azure/function_test_api/Function1.cs
+++ 10_functions_azure/function_test_api/Function1.cs
@@ -21,6 +21,9 @@ namespace function_test_api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // random phrase
             string[] arr = new string[] {"1 - Many hands make light work.",
                 "2 - Strike while the iron is hot.",
                 "3 - Honesty is the best policy.",
@@ -30,45 +33,65 @@ namespace function_test_api
             Random rnd = new Random();
             int value = rnd.Next(1, 5);
 
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            //log.LogInformation(req.Path);
-            //log.LogInformation("1 Lenght: " + req.Path.ToString());
-
+            // day of week
             DateTime currentDateTime = DateTime.Now;
-            //log.LogInformation(currentDateTime.ToString());
-            log.LogInformation("Today is " + currentDateTime.DayOfWeek.ToString());
+            string dayOfWeek = currentDateTime.DayOfWeek.ToString();
+            log.LogInformation("Today is " + dayOfWeek);
 
+
+            // status
+            string[] statusArr =
+            {
+                req.Path.ToString(),
+                req.Body.ToString(),
+                req.ContentLength.ToString(),
+                req.Host.ToString(),
+                req.HttpContext.ToString(),
+                req.IsHttps.ToString(),
+                req.Method.ToString(),
+                req.Protocol.ToString()
+            };
             string name = req.Query["name"];
             
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            Console.OutputEncoding = Encoding.UTF8;
+            var myRequest = new OutputJSON(arr[value], dayOfWeek, statusArr);
+            string json = JsonConvert.SerializeObject(myRequest);
+            log.LogInformation(json);
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? $"Sorry, parameter is empty"
-                : $"Hello, {name}\n\n" +
+                : $"Hello {name}\n" +
+                $"You data:\n" +
+                $"{myRequest} {JsonConvert.DeserializeObject(json)}";
+                /*$"Hello, {name}\n\n" +
                 $"Random phrase:\n {arr[value]}\n\n" +
                 $"Today is {currentDateTime.DayOfWeek.ToString()}\n\n" +
                 $"Path: {req.Path.ToString()}\n" +
                 $"Text: {req.Body.ToString()}\n" +
                 $"Title: {req.ContentLength.ToString()}\n" +
-                //$"{req.ContentType.ToString()}\n";// +
-                //$"{req.Cookies.ToString()}\n";// +
-                //$"{req.HasFormContentType.ToString()}\n" +
-                //$"{req.Headers.ToString()}\n" +
                 $"Host: {req.Host.ToString()}\n" +
                 $"HttpContext: {req.HttpContext.ToString()}\n" +
                 $"IsHttps: {req.IsHttps.ToString()}\n" +
                 $"Method: {req.Method.ToString()}\n" +
-                $"Protocol: {req.Protocol.ToString()}\n";// +
-                //$"{req.Protocol.ToString()}\n" +
-                //$"{req.Query.ToString()}\n" +
-                //$"{req.QueryString.ToString()}\n" +
-                //$"{req.Scheme.ToString()}\n";
+                $"Protocol: {req.Protocol.ToString()}\n";*/
 
             return new OkObjectResult(responseMessage);
+        }
+    }
+
+    public class OutputJSON
+    {
+        public string? Phrase { get; set; }
+        public string? DayOfWeek { get; set; }
+        public Array Status { get; set; }
+        public OutputJSON(string phrase, string dayNow, Array status)
+        {
+            Phrase = phrase;
+            DayOfWeek = dayNow;
+            Status = status;
         }
     }
 }
